9e5fd71b244f7b34d638c700a240e3186ddfd2a9
Mark Cavage <mcavage@gmail.com>
2012-03-31 14:20:42 -0700

    slide updates for dconf

---
+++ dtrace_conf_2012-04-03/demo/intro2.js
@@ -0,0 +1,19 @@
+var restify = require('restify');
+var server = restify.createServer();
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.authorizationParser());
+server.use(restify.dateParser());
+server.use(restify.queryParser());
+server.use(restify.urlEncodedBodyParser());
+
+server.use(function slowHandler(req, res, next) {
+  setTimeout(function() { return next(); }, 513);
+});
+
+server.get('/hello/:name', function echo(req, res, next) {
+  res.send({
+    hello: req.params.name
+  });
+});
+
+server.listen(8080);
